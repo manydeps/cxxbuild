4e96caa487a0cb5133ac4d75d5f81515d19fd89b
Igor Machado <igor.machado@gmail.com>
2023-12-31 13:47:02 +0000

    ready to bumpver again

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cxxbuild',
+    version='1.3.1',
+    packages=['cxxbuild'],
+    entry_points={
+        'console_scripts': [
+            'cxxbuild = cxxbuild.cxxbuild:main',
+        ],
+    },
+)
