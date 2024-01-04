70aca241a82d96d8ea79471243aca4c811e9cdc7
Igor Machado <igor.machado@gmail.com>
2024-01-04 19:49:44 +0000

    bump version 1.5.0 -> 1.5.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Igor Machado Coelho'
 author = 'Igor Machado Coelho'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.5.0'
+release = 'v1.5.1'
 
 
 # -- General configuration ---------------------------------------------------
