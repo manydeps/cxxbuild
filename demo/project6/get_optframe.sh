3334cefedaf455fae74fc78ba6f0497a92d94cce
Igor Machado <igor.machado@gmail.com>
2024-05-21 17:01:22 +0000

    fix download script for demo6

--- demo/project6/get_optframe.sh
+++ demo/project6/get_optframe.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-(cd thirdparty && curl -o master.zip https://github.com/optframe/optframe/archive/refs/heads/master.zip && unzip master.zip)
+curl -L -o thirdparty/master.zip https://github.com/optframe/optframe/archive/refs/heads/master.zip 
+unzip thirdparty/master.zip
+mv optframe-master thirdparty/
