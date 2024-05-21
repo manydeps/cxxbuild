de4715879bfe378b318f4cf2b1e7e78d7f848dfe
Igor Machado <igor.machado@gmail.com>
2024-05-21 14:24:01 +0000

    change wget to curl

--- demo/project6/get_optframe.sh
+++ demo/project6/get_optframe.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(cd thirdparty && wget https://github.com/optframe/optframe/archive/refs/heads/master.zip && unzip master.zip)
+(cd thirdparty && curl -o master.zip https://github.com/optframe/optframe/archive/refs/heads/master.zip && unzip master.zip)
